d1f2960db10adf57d0546191d0a475427cb23ec2
EddieDL <mdressel@gmail.com>
2022-02-18 20:52:28 -0500

    Updates to support REV motors

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -8,37 +8,48 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
-import com.ctre.phoenix.motorcontrol.can.BaseTalonPIDSetConfiguration;
-import com.ctre.phoenix.motorcontrol.can.SlotConfiguration;
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.GeneralConstants;
 import frc.robot.Constants.ShooterConstants;
 
 public class ShooterSubsystem extends SubsystemBase { 
-  public final WPI_TalonSRX popperMotor = new WPI_TalonSRX(ShooterConstants.kPopperCanId);
-  public final WPI_TalonFX bottomMotor = new WPI_TalonFX(ShooterConstants.kBottomCanId);
-  public final WPI_TalonFX topMotor = new WPI_TalonFX(ShooterConstants.kTopCanId);
+  public final CANSparkMax hoodMotor = new CANSparkMax(ShooterConstants.kHoodCanId, MotorType.kBrushless);
+  public final CANSparkMax popperMotor = new CANSparkMax(ShooterConstants.kPopperCanId, MotorType.kBrushless);
+  public final WPI_TalonFX turretMotor = new WPI_TalonFX(ShooterConstants.kTurretCanId, GeneralConstants.kCanBusName);
+  public final WPI_TalonFX bottomMotor = new WPI_TalonFX(ShooterConstants.kBottomCanId, GeneralConstants.kCanBusName);
+  public final WPI_TalonFX topMotor = new WPI_TalonFX(ShooterConstants.kTopCanId, GeneralConstants.kCanBusName);
 
   // Measured Max Velocity 19900;
   final int maxVel = 14900; 
 
   /** Creates a new ExampleSubsystem. */
   public ShooterSubsystem() {
+    // Reset all motors to factory default
+    turretMotor.configFactoryDefault();
     bottomMotor.configFactoryDefault();
     topMotor.configFactoryDefault();
-    popperMotor.configFactoryDefault();
+    hoodMotor.restoreFactoryDefaults(true);
+    popperMotor.restoreFactoryDefaults(true);
     
+    // Configure hood motor
+    hoodMotor.setIdleMode(IdleMode.kBrake);
+
+    // Configure popper motor
+    popperMotor.setIdleMode(IdleMode.kCoast);
+
+    // Configure shooter motors
     bottomMotor.setInverted(InvertType.InvertMotorOutput);
     topMotor.setInverted(InvertType.None);
 
-    TalonFXConfiguration config = getCommonShooterMotorConfig();
+    TalonFXConfiguration config = getShooterMotorConfig();
 
     configShooterMotor(topMotor, config);
     configShooterMotor(bottomMotor, config);
@@ -49,7 +60,7 @@ public class ShooterSubsystem extends SubsystemBase {
     motor.configAllSettings(config);
   }
 
-  private TalonFXConfiguration getCommonShooterMotorConfig() {
+  private TalonFXConfiguration getShooterMotorConfig() {
     TalonFXConfiguration config = new TalonFXConfiguration();
 
     config.supplyCurrLimit.enable = true;
@@ -79,15 +90,16 @@ public class ShooterSubsystem extends SubsystemBase {
     //topMotor.set(TalonFXControlMode.Velocity, maxVel * 1);
     //bottomMotor.set(TalonFXControlMode.Velocity, maxVel * .465);
 
-    popperMotor.set(ControlMode.PercentOutput, .80);
+    popperMotor.set(.8);
   }
 
   public void stop() {
     bottomMotor.set(ControlMode.PercentOutput, 0);
     topMotor.set(ControlMode.PercentOutput, 0);
 
-    popperMotor.set(ControlMode.PercentOutput, 0);
+    popperMotor.set(0);
   }
+
 
   @Override
   public void periodic() {
